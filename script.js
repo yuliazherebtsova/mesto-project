395435a94c3ee34b68a9eeb874accdecc472d2ce
Yulia Zherebtsova <juliaz1206@yandex.ru>
2021-07-25 21:46:28 +0300

    feat: функционал удаления карточки

--- script.js
+++ script.js
@@ -13,53 +13,75 @@ const cardContainer = container.querySelector('.cards__list');
 
 editProfileButton.addEventListener('click', function () {
   // открытие окна редактирования профиля
+
   editForm.elements['name'].value = profileTitle.textContent;
   editForm.elements['occupation'].value = profileSubtitle.textContent;
   // отображаем в окне уже введенную информацию о профиле
+
   editProfilePopup.classList.add('popup_opened');
+
   editProfilePopup.querySelector('.popup__close-button').addEventListener('click', function () {
     editProfilePopup.classList.remove('popup_opened');
   });
+
 });
 
 addCardButton.addEventListener('click', function () {
-  debugger;
   // открытие формы добавления карточки
-  // addForm.elements['place'].value = '';
-  // addForm.elements['picture'].value = '';
+
+  addForm.elements['place'].value = '';
+  addForm.elements['picture'].value = '';
+
   addCardPopup.classList.add('popup_opened');
+
   addCardPopup.querySelector('.popup__close-button').addEventListener('click', function () {
     addCardPopup.classList.remove('popup_opened');
   });
+
 });
 
 editForm.addEventListener('submit', function (evt) {
   // редактирование и сохранение данных профиля
   evt.preventDefault();
   // отменяет стандартную отправку формы, мы можем определить свою логику отправки
+
   profileTitle.textContent = editForm.elements['name'].value;
   profileSubtitle.textContent = editForm.elements['occupation'].value;
+
   editProfilePopup.classList.remove('popup_opened');
+
 });
 
 function addCard(placeValue, pictureSrc) {
   // создание карточки
   const cardTemplate = document.querySelector('#card-template').content;
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
   cardElement.querySelector('.card__title').textContent = placeValue;
   cardElement.querySelector('.card__image').src = pictureSrc;
   cardElement.querySelector('.card__image').alt = placeValue;
+
   cardElement.querySelector('.card__like-button').addEventListener('click', function (evt) {
     evt.target.classList.toggle('card__like-button_active');
   });
+
+  cardElement.querySelector('.card__delete-button').addEventListener('click', function (evt) {
+    evt.target.parentElement.remove();
+  });
+
   cardContainer.prepend(cardElement);
+
 }
 
 addForm.addEventListener('submit', function (evt) {
   // добавление карточки в разметку
   evt.preventDefault();
+
   const placeName = addForm.elements['place'].value;
   const pictureSrc = addForm.elements['picture'].value;
+
   addCard(placeName, pictureSrc);
+
   addCardPopup.classList.remove('popup_opened');
+
 });
