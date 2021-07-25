b2c9154daf05014a9a4d68f4352846752b0aeb93
Yulia Zherebtsova <juliaz1206@yandex.ru>
2021-07-25 16:12:28 +0300

    feat: редактирование имени и информации о себе

--- script.js
+++ script.js
@@ -4,14 +4,26 @@ const profileSubtitle = profile.querySelector('.profile__subtitle')
 const editProfileButton = profile.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const closePopupButton = popup.querySelector('.popup__close-button');
-const editForm = document.getElementById('editForm');
+const editForm = document.querySelector('#editForm');
 
 editProfileButton.addEventListener('click', function () {
   editForm.elements['name'].value = profileTitle.textContent;
   editForm.elements['occupation'].value = profileSubtitle.textContent;
+  // отображаем в окне уже введенную информацию о профиле
   popup.classList.add('popup_opened');
 });
 
 closePopupButton.addEventListener('click', function () {
+  popup.classList.remove('popup_opened');
+});
+
+// Прикрепляем обработчик к форме, он будет следить за событием “submit” - «отправка»
+editForm.addEventListener('submit', function (evt) {
+  evt.preventDefault();
+  // Эта строчка отменяет стандартную отправку формы.
+  // Так мы можем определить свою логику отправки.
+
+  profileTitle.textContent = editForm.elements['name'].value;
+  profileSubtitle.textContent = editForm.elements['occupation'].value;
   popup.classList.remove('popup_opened');
 });
