68ff06483419b43349f4e7d8e36aa729c3c07b5e
Yulia Zherebtsova <juliaz1206@yandex.ru>
2021-07-29 00:47:57 +0300

    fix: refactoring

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-/*
-* 2. чеклист
-* 3. валидатор
-*/
-const container = document.querySelector('.content');
-const profile = container.querySelector('.profile')
-const profileTitle = profile.querySelector('.profile__title')
-const profileSubtitle = profile.querySelector('.profile__subtitle')
+const profile = document.querySelector('.profile');
+const profileTitle = profile.querySelector('.profile__title');
+const profileSubtitle = profile.querySelector('.profile__subtitle');
 const editProfileButton = profile.querySelector('.profile__edit-button');
 const addCardButton = profile.querySelector('.profile__add-button');
 const editProfilePopup = document.querySelector('.popup_type_edit-profile');
 const imagePreviewPopup = document.querySelector('.popup_type_image-preview');
 const addCardPopup = document.querySelector('.popup_type_add-card');
-const editForm = document.querySelector('#editForm');
-const addForm = document.querySelector('#addForm');
-const cardContainer = container.querySelector('.cards__list');
+const editFormName = document.querySelector('#editForm').elements['name'];
+const editFormOccupation = document.querySelector('#editForm').elements['occupation'];
+const addFormPlace = document.querySelector('#addForm').elements['place'];
+const addFormPicture = document.querySelector('#addForm').elements['picture'];
+const cardContainer = document.querySelector('.cards__list');
+const popupCloseButtons = document.querySelectorAll('.popup__close-button');
 
 const initialCards = [
   {
@@ -42,132 +40,133 @@ const initialCards = [
   }
 ];
 
-editProfileButton.addEventListener('click', function () {
-  //  открытие окна редактирования профиля
+function openPopup(popup) {
+  // функция открытия диалогового окна
+  popup.classList.add('popup_opened');
+}
 
-  editForm.elements['name'].value = profileTitle.textContent;
-  editForm.elements['occupation'].value = profileSubtitle.textContent;
-  // отображаем в окне уже введенную информацию о профиле
+function closePopup(popup) {
+  // функция закрытия диалогового окна
+  popup.classList.remove('popup_opened');
+}
 
-  editProfilePopup.classList.add('popup_opened');
-  // отображаем попап
-
-});
-
-addCardButton.addEventListener('click', function () {
-  // открытие формы добавления карточки
-
-  addCardPopup.classList.add('popup_opened');
-  // отображаем попап
-});
-
-editForm.addEventListener('submit', function (evt) {
-  // редактирование и сохранение данных профиля
-
-  evt.preventDefault();
-  // отменяет стандартную отправку формы, которая перезагружает страницу,
-  // теперь можем определить свою логику отправки
-
-  profileTitle.textContent = editForm.elements['name'].value;
-  profileSubtitle.textContent = editForm.elements['occupation'].value;
-  // сохраняем введенные данные в блоке информации о профиле
-
-  editProfilePopup.classList.remove('popup_opened');
-  // закрываем попап
-});
-
-addForm.addEventListener('submit', function (evt) {
-  // добавление карточки в разметку
-
-  evt.preventDefault();
-
-  const placeName = addForm.elements['place'].value;
-  const pictureSrc = addForm.elements['picture'].value;
-
-  addCard(placeName, pictureSrc);
-  // добавляем новую карточку в разметку
-
-  addCardPopup.classList.remove('popup_opened');
-  // закрываем попап
-
-  addForm.elements['place'].value = '';
-  addForm.elements['picture'].value = '';
-  // после добавлении новой карточки поля формы очищаются
-});
-
-editProfilePopup.querySelector('.popup__close-button').addEventListener('click', function () {
-  // добавляем слушатель событию нажатия на кнопку закрытия окна редактирования профиля
-  editProfilePopup.classList.remove('popup_opened');
-});
-
-addCardPopup.querySelector('.popup__close-button').addEventListener('click', function () {
-  // добавляем слушатель событию нажатия на кнопку закрытия окна добавления карточки
-  addCardPopup.classList.remove('popup_opened');
-});
-
-imagePreviewPopup.querySelector('.popup__close-button').addEventListener('click', function () {
-  // добавляем слушатель событию нажатия на кнопку закрытия окна предпросмотра фото
-  imagePreviewPopup.classList.remove('popup_opened');
-});
-
-function addCard(placeValue, pictureSrc) {
+function createCard(placeValue, pictureSrc) {
   // функция создания карточки
-
   const cardTemplate = document.querySelector('#card-template').content;
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
   // создаем новую карточку по шаблону
-
   cardElement.querySelector('.card__title').textContent = placeValue;
   cardElement.querySelector('.card__image').src = pictureSrc;
   cardElement.querySelector('.card__image').alt = placeValue;
   // заполняем шаблон карточки данными из формы создания
 
-  cardElement.querySelector('.card__like-button').addEventListener('click', function (evt) {
+  cardElement.querySelector('.card__like-button').addEventListener('click', (evt) => {
     // создаем слушатель на событие постановки / снятия лайка
     evt.target.classList.toggle('card__like-button_active');
   });
 
-  cardElement.querySelector('.card__delete-button').addEventListener('click', function (evt) {
+  cardElement.querySelector('.card__delete-button').addEventListener('click', (evt) => {
     // создаем слушатель на событие нажатия на кнопку "Удалить" в карточке
     evt.target.parentElement.remove();
   });
 
-  cardElement.querySelector('.card__image').addEventListener('click', function (evt) {
-    debugger;
-    // создаем слушатель на событие нажатия на фото в карточке
-
+  cardElement.querySelector('.card__image').addEventListener('click', (evt) => {
+    // создаем слушатель на событие нажатия на превью фото в карточке
     const clickedImageSrc = evt.target.src;
     const clickedImageTitle = evt.target.nextElementSibling.textContent;
     renderImagePreview(clickedImageSrc, clickedImageTitle);
-    // отображаем попап с выбранной фото
-
   });
 
-  cardContainer.prepend(cardElement);
-  // добавляем карточку в начало списка
+  return cardElement;
+}
+
+function renderCard(card) {
+  // функция добавления новой карточки в разметку
+  cardContainer.prepend(card);
 }
 
 function loadInitialCards(cards) {
-  // функция загрузки начальных карточек
-
+  // функция загрузки начальных карточек на страницу
   cards.forEach(element => {
-    addCard(element.name, element.link);
+    const newCard = createCard(element.name, element.link);
+    renderCard(newCard);
   });
 };
 
+loadInitialCards(initialCards);
+// при загрузке страницы загружаем карточки из заранее заготовленного массива
+
 function renderImagePreview(image, title) {
   // функция отрисовки окна предпросмотра фото
-
   const popupImage = imagePreviewPopup.querySelector('.popup__image');
   const popupImageTitle = imagePreviewPopup.querySelector('.popup__image-title');
-
   popupImage.src = image;
   popupImage.alt = title;
   popupImageTitle.textContent = title;
-
-  imagePreviewPopup.classList.add('popup_opened');
-  // открываем попап предпросмотра фото
+  openPopup(imagePreviewPopup);
+  // открываем окно предпросмотра фото
 }
 
-loadInitialCards(initialCards);
-// при загрузке страницы загружаем карточки из заранее заготовленного массива
+editProfileButton.addEventListener('click', () => {
+  //  открытие окна редактирования профиля
+  editFormName.value = profileTitle.textContent;
+  editFormOccupation.value = profileSubtitle.textContent;
+  // отображаем в окне уже введенную информацию о профиле
+  openPopup(editProfilePopup);
+});
+
+addCardButton.addEventListener('click', () => {
+  // открытие формы добавления карточки
+  openPopup(addCardPopup);
+});
+
+editForm.addEventListener('submit', (evt) => {
+  // редактирование и сохранение данных профиля
+  const popup = evt.target.closest('.popup');
+  evt.preventDefault();
+  // отменяет стандартную отправку формы, которая перезагружает страницу,
+  // теперь можем определить свою логику отправк
+  profileTitle.textContent = editFormName.value;
+  profileSubtitle.textContent = editFormOccupation.value;
+  // сохраняем введенные данные в блоке информации о профиле
+  closePopup(popup);
+});
+
+addForm.addEventListener('submit', (evt) => {
+  // добавление карточки в разметку
+  evt.preventDefault();
+  const placeName = addFormPlace.value;
+  const pictureSrc = addFormPicture.value;
+  const newCard = createCard(placeName, pictureSrc);
+  // создаем новую карточку
+  renderCard(newCard);
+  // добавляем карточку на страницу в начало списка
+  addFormPlace.value = '';
+  addFormPicture.value = '';
+  // после добавлении новой карточки поля формы очищаются,
+  // новая форма не значений, введенных ранее
+  closePopup(addCardPopup);
+});
+
+function setMultipleEventListeners(elements) {
+  // функция добавления слушателей для события клика сразу нескольким DOM элементам
+  const elementsArray = Array.from(elements);
+  elementsArray.forEach(element => element.addEventListener('click', (evt) => {
+    closePopup(evt.target.closest('.popup'));
+  }));
+}
+
+setMultipleEventListeners(popupCloseButtons);
+// добавляем слушателей кнопкам закрытия для всех диалоговых окон в разметке
+
+addCardPopup.querySelector('.popup__close-button').addEventListener('click', (evt) => {
+  // добавляем слушатель событию нажатия на кнопку закрытия окна добавления карточки
+  const popup = evt.target.closest('.popup');
+  closePopup(popup);
+});
+
+imagePreviewPopup.querySelector('.popup__close-button').addEventListener('click', (evt) => {
+  // добавляем слушатель событию нажатия на кнопку закрытия окна предпросмотра фото
+  const popup = evt.target.closest('.popup');
+  closePopup(popup);
+});
