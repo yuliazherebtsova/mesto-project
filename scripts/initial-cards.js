06286610cf0e8e021f802fcf41617037970c4b9c
Yulia Zherebtsova <juliaz1206@yandex.ru>
2021-07-29 21:52:46 +0300

    fix: данные начальных карточек вынесены в отдельный js-файл

---
+++ scripts/initial-cards.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    name: 'Барселона',
+    link: './images/cards-barcelona.jpg'
+  },
+  {
+    name: 'Париж, Франция',
+    link: './images/cards-paris.jpg'
+  },
+  {
+    name: 'Тбилиси, Грузия',
+    link: './images/cards-tbilisi.jpg'
+  },
+  {
+    name: 'Таллин, Эстония',
+    link: './images/cards-tallin.jpg'
+  },
+  {
+    name: 'Базель, Швейцария',
+    link: './images/cards-basel.jpg'
+  },
+  {
+    name: 'Гент, Бельгия',
+    link: './images/cards-gent.jpg'
+  }
+];
--- scripts/script.js
+++ scripts/script.js
@@ -15,33 +15,6 @@ const formAddPictureField = document.querySelector('#formAddCard').elements['pic
 const cardContainer = document.querySelector('.cards__list');
 const popupCloseButtons = document.querySelectorAll('.popup__close-button');
 
-const initialCards = [
-  {
-    name: 'Барселона',
-    link: './images/cards-barcelona.jpg'
-  },
-  {
-    name: 'Париж, Франция',
-    link: './images/cards-paris.jpg'
-  },
-  {
-    name: 'Тбилиси, Грузия',
-    link: './images/cards-tbilisi.jpg'
-  },
-  {
-    name: 'Таллин, Эстония',
-    link: './images/cards-tallin.jpg'
-  },
-  {
-    name: 'Базель, Швейцария',
-    link: './images/cards-basel.jpg'
-  },
-  {
-    name: 'Гент, Бельгия',
-    link: './images/cards-gent.jpg'
-  }
-];
-
 function openPopup(popup) {
   // функция открытия диалогового окна
   popup.classList.add('popup_opened');
